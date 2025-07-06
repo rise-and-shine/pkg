6b722ed4d05b390a07f06a866692ca31d8afc689
quvonchbek <quvonchbek.o@iman.uz>
2025-07-06 12:05:34 +0500

    aded printConfig func

--- cfgloader/cfgloader.go
+++ cfgloader/cfgloader.go
@@ -3,6 +3,7 @@ package cfgloader
 
 import (
 	"fmt"
+	"log"
 	"log/slog"
 	"os"
 	"reflect"
@@ -63,6 +64,8 @@ func MustLoad[T any]() T {
 	setDefaults(&config)
 
 	validateConfig(&config, env)
+
+	printConfig(&config)
 
 	return config
 }
@@ -155,4 +158,110 @@ func validateConfig(config any, env string) {
 		)
 		os.Exit(1)
 	}
+}
+
+func printConfig(config any) {
+	masked := maskStruct(config)
+
+	out, err := yaml.Marshal(masked)
+	if err != nil {
+		log.Fatalf("failed to marshal config: %v", err)
+	}
+	fmt.Println(string(out))
+}
+
+func maskStruct(cfg any) any {
+	val := reflect.ValueOf(cfg)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return maskValue(val).Interface()
+}
+
+func maskValue(val reflect.Value) reflect.Value {
+	if !val.IsValid() {
+		return val
+	}
+
+	switch val.Kind() {
+	case reflect.Ptr:
+		if val.IsNil() {
+			return val
+		}
+		ptr := reflect.New(val.Elem().Type())
+		ptr.Elem().Set(maskValue(val.Elem()))
+		return ptr
+
+	case reflect.Struct:
+		masked := reflect.New(val.Type()).Elem()
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Type().Field(i)
+			origVal := val.Field(i)
+
+			if !masked.Field(i).CanSet() || !origVal.CanInterface() {
+				continue
+			}
+
+			if field.Tag.Get("mask") == "true" {
+				masked.Field(i).Set(maskAny(origVal))
+			} else {
+				masked.Field(i).Set(maskValue(origVal))
+			}
+		}
+		return masked
+
+	case reflect.Slice:
+		masked := reflect.MakeSlice(val.Type(), val.Len(), val.Cap())
+		for i := 0; i < val.Len(); i++ {
+			masked.Index(i).Set(maskValue(val.Index(i)))
+		}
+		return masked
+
+	case reflect.Array:
+		masked := reflect.New(val.Type()).Elem()
+		for i := 0; i < val.Len(); i++ {
+			masked.Index(i).Set(maskValue(val.Index(i)))
+		}
+		return masked
+
+	case reflect.Map:
+		masked := reflect.MakeMapWithSize(val.Type(), val.Len())
+		for _, key := range val.MapKeys() {
+			masked.SetMapIndex(key, maskValue(val.MapIndex(key)))
+		}
+		return masked
+
+	case reflect.Interface:
+		if val.IsNil() {
+			return val
+		}
+		return maskValue(val.Elem())
+
+	case reflect.String:
+		return reflect.ValueOf(maskString(val.String()))
+
+	default:
+		return val
+	}
+}
+
+func maskAny(val reflect.Value) reflect.Value {
+	if !val.IsValid() {
+		return val
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		return reflect.ValueOf(maskString(val.String()))
+
+	case reflect.Struct, reflect.Slice, reflect.Array, reflect.Map, reflect.Interface, reflect.Ptr:
+		return maskValue(val)
+
+	default:
+		return reflect.Zero(val.Type())
+	}
+}
+
+func maskString(s string) string {
+	return strings.Repeat("*", len(s))
 }
