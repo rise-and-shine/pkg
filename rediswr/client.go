8bde346576760fb710112610c5b10c0a316d50b5
Nuriddin Obidjonov <nuriddin.obidjonov777@gmail.com>
2025-12-12 10:25:41 +0500

    impl rediswr package

--- rediswr/client.go
+++ rediswr/client.go
@@ -4,12 +4,15 @@ import (
 	"strings"
 
 	"github.com/redis/go-redis/v9"
+	"github.com/rise-and-shine/pkg/meta"
 )
 
 // New creates a new Redis client.
 func New(cfg Config) redis.Cmdable {
 	client := redis.NewUniversalClient(&redis.UniversalOptions{
 		Addrs:         strings.Split(cfg.Addrs, ","),
+		ClientName:    meta.GetServiceName(),
+		Username:      cfg.Username,
 		Password:      cfg.Password,
 		IsClusterMode: cfg.IsClusterMode,
 	})
--- rediswr/config.go
+++ rediswr/config.go
@@ -6,6 +6,9 @@ type Config struct {
 	// Addrs is the list of Redis server addresses in the format "host:port,host2:port2".
 	Addrs string `yaml:"addrs" validate:"required"`
 
+	// Username is the username for the Redis server/cluster.
+	Username string `yaml:"username"`
+
 	// Password is the password for the Redis server/cluster.
 	Password string `yaml:"password"`
 
