cfafbd7f9ab51b4eac2dd85b36705515954353d6
Nuriddin Obidjonov <nuriddin.obidjonov777@gmail.com>
2026-01-14 12:47:29 +0500

    impl token package

--- token/opaque.go
+++ token/opaque.go
@@ -11,6 +11,7 @@ func NewOpaqueToken() string {
 	b := make([]byte, 32)
 	_, err := rand.Read(b)
 	if err != nil {
+		// this should never happen
 		panic(fmt.Errorf("failed to generate opaque token: %w", err))
 	}
 	return base64.URLEncoding.WithPadding(base64.NoPadding).EncodeToString(b)
